9ed3fd43be042a4a1288e3aacb98bf7201bfdce2
ReoYabiku <alcremikyu.z@gmail.com>
2023-03-06 00:15:36 +0900

    誤字修正

--- public/main.js
+++ public/main.js
@@ -17,24 +17,24 @@ window.onload = function() {
   let hidden = document.getElementById('hidden');
 
   menu_about.addEventListener('click', function() {
-    hidden.clickek = false;
+    hidden.clicked = false;
     console.log('clicked!');
     location.reload;
   })
   menu_philosophy.addEventListener('click', function() {
-    hidden.clickek = false;
+    hidden.clicked = false;
     location.reload;
   })
   menu_biography.addEventListener('click', function() {
-    hidden.clickek = false;
+    hidden.clicked = false;
     location.reload;
   })
   menu_achievement.addEventListener('click', function() {
-    hidden.clickek = false;
+    hidden.clicked = false;
     location.reload;
   })
   menu_contact.addEventListener('click', function() {
-    hidden.clickek = false;
+    hidden.clicked = false;
     location.reload;
   })
 }
