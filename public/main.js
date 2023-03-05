4de576c27158c6cdc94aecb20c0223fc84e4a28e
ReoYabiku <alcremikyu.z@gmail.com>
2023-03-05 23:53:11 +0900

    jsでメニューを消す

--- public/main.js
+++ public/main.js
@@ -3,3 +3,29 @@ window.addEventListener('load', function() {
     top: -40,
   });
 });
+
+window.onload = function() {
+  let menu_about = document.getElementById('menu-about');
+  let menu_philosophy = document.getElementById('menu-philosophy');
+  let menu_biography = document.getElementById('menu-biography');
+  let menu_achievement = document.getElementById('menu-achievement');
+  let menu_contact = document.getElementById('menu-contact');
+
+  let hidden = document.getElementById('hidden');
+
+  menu_about.addEventListener('click', function() {
+    hidden.clickek = false;
+  })
+  menu_philosophy.addEventListener('click', function() {
+    hidden.clickek = false;
+  })
+  menu_biography.addEventListener('click', function() {
+    hidden.clickek = false;
+  })
+  menu_achievement.addEventListener('click', function() {
+    hidden.clickek = false;
+  })
+  menu_contact.addEventListener('click', function() {
+    hidden.clickek = false;
+  })
+}
