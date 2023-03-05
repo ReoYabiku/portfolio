8d5c069568d72c74fccb06b6539d17cfe819bc00
ReoYabiku <alcremikyu.z@gmail.com>
2023-03-06 00:41:13 +0900

    /の有無の検証

--- public/main.js
+++ public/main.js
@@ -4,40 +4,40 @@ window.addEventListener('load', function() {
   });
 });
 
-window.onload = function() {
-  const menu_about = document.getElementById('menu-about');
-  if(menu_about != null) {
-    console.log("not null");
-  }
-  const menu_philosophy = document.getElementById('menu-philosophy');
-  const menu_biography = document.getElementById('menu-biography');
-  const menu_achievement = document.getElementById('menu-achievement');
-  const menu_contact = document.getElementById('menu-contact');
+// window.onload = function() {
+//   const menu_about = document.getElementById('menu-about');
+//   if(menu_about != null) {
+//     console.log("not null");
+//   }
+//   const menu_philosophy = document.getElementById('menu-philosophy');
+//   const menu_biography = document.getElementById('menu-biography');
+//   const menu_achievement = document.getElementById('menu-achievement');
+//   const menu_contact = document.getElementById('menu-contact');
 
-  const menu = document.getElementById('menu');
-  let shadow = document.getElementById('shadow');
+//   const menu = document.getElementById('menu');
+//   let shadow = document.getElementById('shadow');
 
-  console.log(menu.style.display);
+//   console.log(menu.style.display);
 
-  menu_about.addEventListener('click', function() {
-    menu.style.display = 'none';
-    shadow.style.display = 'none';
-    console.log('clicked!');
-  })
-  menu_philosophy.addEventListener('click', function() {
-    menu.style.display = 'none';
-    shadow.style.display = 'none';
-  })
-  menu_biography.addEventListener('click', function() {
-    menu.style.display = 'none';
-    shadow.style.display = 'none';
-  })
-  menu_achievement.addEventListener('click', function() {
-    menu.style.display = 'none';
-    shadow.style.display = 'none';
-  })
-  menu_contact.addEventListener('click', function() {
-    menu.style.display = 'none';
-    shadow.style.display = 'none';
-  })
-}
+//   menu_about.addEventListener('click', function() {
+//     menu.style.display = 'none';
+//     shadow.style.display = 'none';
+//     console.log('clicked!');
+//   })
+//   menu_philosophy.addEventListener('click', function() {
+//     menu.style.display = 'none';
+//     shadow.style.display = 'none';
+//   })
+//   menu_biography.addEventListener('click', function() {
+//     menu.style.display = 'none';
+//     shadow.style.display = 'none';
+//   })
+//   menu_achievement.addEventListener('click', function() {
+//     menu.style.display = 'none';
+//     shadow.style.display = 'none';
+//   })
+//   menu_contact.addEventListener('click', function() {
+//     menu.style.display = 'none';
+//     shadow.style.display = 'none';
+//   })
+// }
