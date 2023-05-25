2b97d77970e65f21c4da194a62e14193b78d60cd
ReoYabiku <alcremikyu.z@gmail.com>
2023-05-25 12:15:01 +0900

    closeHumburgerMenuによるリファクタリング

--- public/main.js
+++ public/main.js
@@ -1,49 +1,20 @@
-window.onload = function() {
-  const menu_about = document.getElementById('menu-about');
-  const menu_philosophy = document.getElementById('menu-philosophy');
-  const menu_biography = document.getElementById('menu-biography');
-  const menu_achievement = document.getElementById('menu-achievement');
-  const menu_contact = document.getElementById('menu-contact');
+const menu = document.getElementById('menu');
+const shadow = document.getElementById('shadow');
+const hidden = document.getElementById('hidden');
 
-  const menu = document.getElementById('menu');
-  const shadow = document.getElementById('shadow');
-  const hidden = document.getElementById('hidden');
+function main() {
+  const menu_ids = [
+    '#menu-about',
+    '#menu-philosophy',
+    '#menu-biography',
+    '#menu-achievement',
+    '#menu-contact'
+  ];
+  menu_ids.forEach(menu_id => {
+    document.querySelector(menu_id).addEventListener('click', closeHumburgerMenu);
+  });
 
-  menu_about.addEventListener('click', function() {
-    console.log("menu_about was clicked!!");
-    menu.style.display = 'none';
-    shadow.style.display = 'none';
-    hidden.checked = false;
-    location.reload();
-  })
-  menu_philosophy.addEventListener('click', function() {
-    menu.style.display = 'none';
-    shadow.style.display = 'none';
-    hidden.checked = false;
-    location.reload();
-  })
-  menu_biography.addEventListener('click', function() {
-    menu.style.display = 'none';
-    shadow.style.display = 'none';
-    hidden.checked = false;
-    location.reload();
-  })
-  menu_achievement.addEventListener('click', function() {
-    menu.style.display = 'none';
-    shadow.style.display = 'none';
-    hidden.checked = false;
-    location.reload();
-  })
-  menu_contact.addEventListener('click', function() {
-    menu.style.display = 'none';
-    shadow.style.display = 'none';
-    hidden.checked = false;
-    location.reload();
-  })
-
-  
   const details_list = document.getElementsByTagName('details');
-  
   if(window.outerWidth > 1300) {
     for(const details of details_list) {
       details.open = true;
@@ -54,3 +25,12 @@ window.onload = function() {
     }
   }
 }
+
+function closeHumburgerMenu() {
+  menu.style.display = 'none';
+  shadow.style.display = 'none';
+  hidden.checked = false;
+  location.reload();
+}
+
+window.onload = main();
